c5d02b33f3e180d25c2de098373a33fa48437d5b
jkwourms <jkw64@case.edu>
2016-12-03 16:20:38 -0500

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,37 +1,64 @@
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.*;
 
-public class GUI extends Format implements ActionListener {
-	
-	private JTextField text;
-	
-	public GUI() {
-		JFrame.setDefaultLookAndFeelDecorated(true);
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//Create search button
-		Button search = new Button("Search");
-		frame.add(search, BorderLayout.EAST);
-		search.addActionListener(this);
-		
-		//Create textbox
-		text = new JTextField("Search for a word", 13);
-		frame.add(text, BorderLayout.WEST);
-		
-		frame.setSize(250, 100);
-		frame.setVisible(true);
-	}
-	
-	//When search button is pressed, console prints the user-inputted word to be searched
-	public void actionPerformed(ActionEvent evt){
-		String word = text.getText();
-		System.out.println(word);
-	}
-	
-	public static void main(String[] args) {
-		new GUI();
-	}
+package pkgfinal.project.netbeans;
+
+/**
+ *
+ * @author jenniferwourms
+ */
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.text.Text;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
+
+public class FinalProjectNetbeans extends Application {
+
+    @Override
+    public void start(Stage startPage) throws Exception {
+        startPage.setTitle("Hello there");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.TOP_CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+         
+        //Creates a title page asking for the word to be searched
+        Text pageTitle = new Text("Looking for a word?");
+        pageTitle.setFont(Font.font("Lora", FontWeight.NORMAL, 20));
+        grid.add(pageTitle, 0, 0, 2, 1);
+        
+        Label fileName = new Label("Enter the name of the file you wish to search:");
+        grid.add(fileName, 0, 1);
+        
+        TextField fileTextField = new TextField();
+        grid.add(fileTextField, 1, 1);      
+        fileTextField.setOnAction(new EventHandler<ActionEvent>() {
+        public void handle(ActionEvent e) {
+            System.out.println(fileTextField.getText());
+            //openFile, readFile, closeFile methods called from Format class
+        }
+        });
+        
+        Scene scene = new Scene(grid, 600, 275);
+        startPage.setScene(scene);
+        startPage.show();
+    
+    
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    
 }
