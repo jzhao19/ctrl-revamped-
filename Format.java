e02f224b3fbc37f502cbe77ccea506add2c2268f
jkwourms <jkw64@case.edu>
2016-12-05 17:03:06 -0500

    Update Format.java
    Now it's its own class weee

--- Format.java
+++ Format.java
@@ -1,45 +1,62 @@
+package pkgfinal.project.netbeans;
+
+
 import java.io.File;
 import java.util.Scanner;
+import pkgfinal.project.netbeans.hash;
 
-public class Format {
+//Format class to load, read, and close the file chosen by the user
+    public class Format {
 	private Scanner input;
-	//private char[] punctuation = {'.', ',', ';', ':', '?', '!', '(', ')', '/', '\'', '&', '^', '$', '#', '@', '~'}; 
-	
-	public static void main(String[] args) {
-		Format file = new Format();
-		file.openFile();
-		file.readFile();
-		file.closeFile();
-	}
-	
-	public void openFile(){
-		try {	
-			input = new Scanner(new File("hashText1.txt"));
-		}
-		catch(Exception e) {
-			System.out.println("Could not find file");
-		}
-	}
-	
-	//This method reads the file, makes all words lower-case, and prints out each word on a separate line
-	public void readFile() {
+
+        public Format() {
+        }
+        
+        public int findKey(String searchedWord) {
+            String hashMe = searchedWord.toUpperCase();
+            int asciiValue = 0;
+            for (int i = 0; i < hashMe.length(); i++) {
+                char character = hashMe.charAt(i);
+                int ascii = (int) character;
+                asciiValue = asciiValue + ascii;
+            }
+            return asciiValue;
+        }
+        
+        
+        public void read(String fileName) {
+            try {
+                input = new Scanner(new File(fileName));
+                System.out.println("Successfully loaded file ");
+                readFile(input);
+            }
+            catch(Exception e) {
+                System.out.println("Could not find file");
+            }
+        }
+        //make all words lower-case
+	public void readFile(Scanner input) {
 		while(input.hasNext()) {
-			String nWord = "";
+                        StringBuilder nWord = new StringBuilder();   
 			String word = input.next();
 			//gets rid of ALL punctuation & numbers -- could change later to include characters like <3
 			for (int i = 0; i < word.length() && !word.matches(".*\\d+.*"); i++) {
 				if (Character.getNumericValue(word.charAt(i)) != -1) {
-					nWord += word.charAt(i);
+					nWord.append(word.charAt(i));
 				}
 			}
-			if (nWord != "") {
-				System.out.println(nWord.toLowerCase() + " ");
-				//insert hash function here!
+			if (nWord.length() > 0) {
+                                int asciiValue = findKey(nWord.toString());
+				//System.out.println(nWord.toString().toUpperCase() + " " + asciiValue);
+				//insert hash function here
+                                hash hTable = new hash();
+                                hTable.insert(asciiValue, nWord.toString().toUpperCase());
 			}
 		}
-	}
+                closeFile(input);
+	} 
 	
-	public void closeFile() {
+	public void closeFile(Scanner input) {
 		input.close();
 	}
-}
+    }
