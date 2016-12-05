05f6e477bdaf51097ec4155d1748014e90c7a3dd
jzhao19 <sxz402@case.edu>
2016-12-05 18:20:42 -0500

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,70 +1,133 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package ctrlf;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author Jenny
  */
-public class Node {
-    private String Str;
-    private Node Next;
-    private Node Prev;
-    private int Keyval;
+public class hash {
+    private static Node data [] = new Node [11];
     
-    public Node (String str, Node next, Node prev){
-    Str = str;
-    Next = next;
-    Prev = prev;
-}
-    public Node setnext(Node next){
-        Next = next;
-        return Next;
-    }
-    
-    public Node getnext(){
-        return Next;
-    }
-    
-    public Node setprev(Node prev){
-        Prev = prev;
-        return Prev;
-    }
-    
-    public Node getprev(){
-        return Prev;
-    }
-    
-    public int setval(int keyval){
-        Keyval = keyval;
-        return Keyval;
-    }
-    
-    public int getval(){
-        return Keyval;
-    }
-    
-    public String getString(){
-        return Str;
-    }
-    
-    public int compareTo(String name){
-        if (this.getString().compareTo(name) < 1) {
-            return -1;
+    public static Node insert(int key, String Name, int index){
+        int loc = 0;
+        loc = (key) % data.length;
+        if (data[loc] == null){
+            Node curr = new Node(Name,null, null, index);
+            data[loc] = curr;
+            curr.setval(loc);
+            return curr;
         }
-        else if (this.getString().compareTo(name) > 1){
-            return 1;
+        else if (data[loc].getString().compareTo(Name) == 0){
+            Node prv = data[loc];
+            while (prv.getnext() != null){
+                prv = prv.getnext();
+            }
+            //System.out.println(prv.getnext());
+            Node curr = new Node(Name,null, prv, index);
+            prv.setnext(curr);
+            //System.out.println(prv);
+            curr.setval(loc);
+            return curr;
         }
-        else return 0;
+        
+        for (int x = 0; x< data.length; x++){
+        loc = (int) ((x*(7 - (key%7))) + (key % data.length)); 
+        if (loc >= data.length){
+             int locc = (loc % data.length);
+             loc = locc;
+        }
+        if (data[loc] == null){
+            Node curr = new Node(Name,null, null, index);
+            data[loc] = curr;
+            curr.setval(loc);
+            return curr;
+        }
+        else if (data[loc].getString().compareTo(Name) == 0){
+            Node prv = data[loc];
+            while (prv.getnext() != null){
+                prv = prv.getnext();
+            }
+            //System.out.println(prv.getnext());
+            Node curr = new Node(Name,null, prv, index);
+            prv.setnext(curr);
+            //System.out.println(prv);
+            curr.setval(loc);
+            return curr;
+        }
+        }
+       return data[loc]; 
     }
     
-    @Override
-    public String toString(){
-        System.out.println("String:  " + Str + "   Next Node:  " + Next);
-    return Str;
-}
+    public static ArrayList find(String Name, int key){
+        ArrayList <NewString> Found = new ArrayList <NewString>();
+        int loc = (key) % data.length;
+        //System.out.println("loc:  " + loc);
+        Node next = null;
+        if (data[loc]==null){
+            Found.add(new NewString(null,0));
+            return Found;
+        }
+        if (data[loc].getString().compareTo(Name) == 0){
+            System.out.println("in");
+            Found.add(new NewString(data[loc].getString(),data[loc].getIndex()));
+            if(data[loc].getnext()==null){
+                return Found;
+            }
+            next = data[loc].getnext();
+        }
+        else {
+            
+            for (int x = 0; x< data.length; x++){   
+            loc = (int) ((x*(7 - (key%7))) + (key % data.length)); 
+                if (loc >= data.length){
+                int locc = (loc % data.length);
+                loc = locc;
+                }
+                    if (data[loc].getString().compareTo(Name) == 0){
+                    Found.add(new NewString(data[loc].getString(), data[loc].getIndex()));
+                    if(data[loc].getnext()==null){
+                    return Found;
+                    }
+                    next = data[loc].getnext();
+                    System.out.println("thisisus" + next.getString());
+                    break;
+                }
+            }
+        }
+        
+        //System.out.println("loc:  " + loc);
+        
+        if (next == null){
+            Found.add(new NewString(null,0));
+            return Found;
+        }
+
+        while (next!= null){
+            System.out.println("adding");
+            Found.add(new NewString(next.getString(), next.getIndex()));
+            next = next.getnext();
+        }
+        System.out.println(Found.toString());
+        return Found;
+    }
+    
+    public static void printtable(){
+        for (int x = 0; x< data.length;x++){
+            if (data[x] ==null){
+                System.out.println(x + ": " + data[x]);
+            }
+            else{
+                System.out.println(x + ": " + data[x].getString());
+            }
+            while(data[x] != null && data[x].getnext()!= null){
+            System.out.println(data[x].getnext().getString());
+            data[x]=data[x].getnext();
+        }
+
+    }
+    }
+
+
 }
